8887f6217393dae579c4f96d7295ebcca265f86e
Michal Miszczyszyn <michal@mmiszy.pl>
2020-09-07 20:13:52 +0200

    Updates

---
+++ dist-src/v2.js
@@ -0,0 +1,170 @@
+import propertyMapper from "./property-mapper";
+import { comment, nodeType, transformRef, tsArrayOf, tsIntersectionOf, tsUnionOf, } from "./utils";
+export const PRIMITIVES = {
+    boolean: "boolean",
+    binary: "string",
+    byte: "string",
+    date: "string",
+    dateTime: "string",
+    password: "string",
+    string: "string",
+    double: "number",
+    float: "number",
+    integer: "number",
+    number: "number",
+};
+export default function generateTypesV2(schema, options) {
+    if (!schema.definitions) {
+        throw new Error(`⛔️ 'definitions' missing from schema https://swagger.io/specification/v2/#definitions-object`);
+    }
+    const propertyMapped = options
+        ? propertyMapper(schema.definitions, options.propertyMapper)
+        : schema.definitions;
+    function transform(node) {
+        switch (nodeType(node)) {
+            case "ref": {
+                return transformRef(node.$ref);
+            }
+            case "string":
+            case "number":
+            case "boolean": {
+                return nodeType(node) || "any";
+            }
+            case "enum": {
+                return tsUnionOf(node.enum.map((item) => `'${item}'`));
+            }
+            case "object": {
+                if ((!node.properties || !Object.keys(node.properties).length) &&
+                    !node.allOf &&
+                    !node.additionalProperties) {
+                    return `{ [key: string]: any }`;
+                }
+                let properties = definitionsToTypes(node.properties || {}, node.required);
+                if (node.additionalProperties) {
+                    properties += `[key: string]: ${nodeType(node.additionalProperties) || "any"};\n`;
+                }
+                return tsIntersectionOf([
+                    ...(node.allOf ? node.allOf.map(transform) : []),
+                    ...(properties ? [`{ ${properties} }`] : []),
+                ]);
+                break;
+            }
+            case "array": {
+                return tsArrayOf(transform(node.items));
+            }
+        }
+        return "";
+    }
+    function definitionsToTypes(obj, required = []) {
+        if (!obj) {
+            return "";
+        }
+        let output = "";
+        Object.entries(obj).forEach(([key, value]) => {
+            if (value.description) {
+                output += comment(value.description);
+            }
+            output += `"${key}"${!required || !required.includes(key) ? "?" : ""}: `;
+            output += transform(value);
+            output += ";\n";
+        });
+        return output;
+    }
+    function capitalize(str) {
+        return String(str).replace(/^\w/, (l) => l.toUpperCase());
+    }
+    function pathToTypeName(str) {
+        return capitalize(str
+            .replace(/[\/\-{](\w)/g, (_, letter) => letter.toUpperCase())
+            .replace(/\W/g, ""));
+    }
+    function pathsToTypes(obj) {
+        if (!obj) {
+            return "";
+        }
+        let output = "";
+        Object.entries(obj).forEach(([path, pathItemObject]) => {
+            Object.entries(pathItemObject).forEach(([key, operationObject]) => {
+                const method = key;
+                const endpointName = method + pathToTypeName(path);
+                if (operationObject.parameters) {
+                    const groupedParameters = Object.values(operationObject.parameters).reduce((params, parameterObject) => {
+                        if (parameterObject.in === "body") {
+                            params[parameterObject.in] = parameterObject;
+                        }
+                        else {
+                            const newValue = params[parameterObject.in] || {};
+                            newValue[parameterObject.name] = parameterObject;
+                            params[parameterObject.in] = newValue;
+                        }
+                        return params;
+                    }, {});
+                    Object.entries(groupedParameters).forEach(([key, paramGroup]) => {
+                        if (!paramGroup) {
+                            return;
+                        }
+                        const paramGroupName = key;
+                        const groupNameToTypeName = {
+                            body: "Body",
+                            query: "Query",
+                            header: "Headers",
+                            formData: "FormData",
+                            path: "PathParams",
+                        };
+                        if (paramGroupName === "body") {
+                            output += `${endpointName}Request${groupNameToTypeName[paramGroupName]}: `;
+                            output += transform(paramGroup.schema);
+                            output += ";\n";
+                        }
+                        else {
+                            output += `${endpointName}Request${groupNameToTypeName[paramGroupName]}: `;
+                            output += "{\n";
+                            Object.entries(paramGroup).forEach(([, schema]) => {
+                                const parameterObject = schema;
+                                output += `"${parameterObject.name}"${!parameterObject.required ? "?" : ""}: `;
+                                output += transform(parameterObject);
+                                output += ";\n";
+                            });
+                            output += "};\n";
+                        }
+                    });
+                }
+                Object.entries(operationObject.responses).forEach(([key, responseObject]) => {
+                    if (!responseObject) {
+                        return;
+                    }
+                    if ("$ref" in responseObject) {
+                        return;
+                    }
+                    if (!responseObject.schema) {
+                        return;
+                    }
+                    const responseType = key;
+                    output += "\n";
+                    let descriptionAndSummary = "";
+                    if (operationObject.description) {
+                        descriptionAndSummary += `@description ${operationObject.description}`;
+                    }
+                    if (operationObject.summary) {
+                        const responseDescription = responseObject.description || "";
+                        if (descriptionAndSummary) {
+                            descriptionAndSummary += "\n";
+                        }
+                        descriptionAndSummary += `@summary ${operationObject.summary} ${responseType} ${responseDescription} response`;
+                    }
+                    if (descriptionAndSummary) {
+                        output += comment(descriptionAndSummary);
+                    }
+                    output += `${endpointName}${capitalize(responseType)}Response: `;
+                    output += transform(responseObject.schema);
+                    output += ";\n";
+                });
+            });
+        });
+        return output;
+    }
+    return `export interface definitions {
+    ${definitionsToTypes(propertyMapped, Object.keys(propertyMapped))}
+    ${pathsToTypes(schema.paths)}
+  }`;
+}
---
+++ dist-types/types/OpenAPI2.d.ts
@@ -0,0 +1,76 @@
+declare type HttpMethods = "get" | "put" | "post" | "delete" | "options" | "head" | "patch";
+declare type HttpStatuses = 100 | 101 | 102 | 103 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226 | 300 | 301 | 302 | 303 | 304 | 305 | 306 | 307 | 308 | 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 421 | 422 | 423 | 424 | 425 | 426 | 428 | 429 | 431 | 451 | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511;
+export interface OpenAPI2 {
+    definitions?: {
+        [key: string]: OpenAPI2SchemaObject;
+    };
+    swagger: string;
+    paths: OpenAPI2PathsObject;
+    [key: string]: any;
+}
+export declare type OpenAPI2PathsObject = {
+    [path: string]: OpenAPI2PathItemObject;
+};
+declare type OpenAPI2PathItemObject = {
+    [method in HttpMethods]: OpenAPI2OperationObject;
+};
+export declare type OpenAPI2OperationObject = {
+    tags?: string[];
+    summary?: string;
+    description?: string;
+    operationId: string;
+    responses: OpenAPI2ResponsesObject;
+    parameters?: OpenAPI2ParametersObject;
+};
+export declare type OpenAPI2ResponsesObject = {
+    default?: OpenAPI2Reference | OpenAPI2ResponseObject;
+} & {
+    [status in HttpStatuses]: OpenAPI2Reference | OpenAPI2ResponseObject;
+};
+export declare type OpenAPI2ResponseObject = {
+    description: string;
+    schema?: OpenAPI2SchemaObject | OpenAPI2Reference;
+};
+export declare type OpenAPI2ParameterType = "string" | "number" | "integer" | "boolean" | "array" | "file";
+export declare type OpenAPI2ParametersObject = {
+    [k in string | number]: OpenAPI2ParameterObject;
+};
+export declare type OpenAPI2ParameterObject = {
+    name: string;
+    description?: string;
+} & ({
+    in: "body";
+    required?: boolean;
+    schema: OpenAPI2SchemaObject | OpenAPI2Reference;
+} | {
+    in: "query" | "header" | "formData";
+    required?: boolean;
+    type: OpenAPI2ParameterType;
+    enum?: ReadonlyArray<unknown>;
+} | {
+    in: "path";
+    required: true;
+    type: OpenAPI2ParameterType;
+    enum?: ReadonlyArray<unknown>;
+});
+export declare type OpenAPI2Type = "array" | "binary" | "boolean" | "byte" | "date" | "dateTime" | "double" | "float" | "integer" | "long" | "number" | "object" | "password" | "string";
+export declare type OpenAPI2Reference = {
+    $ref: string;
+};
+export interface OpenAPI2SchemaObject {
+    additionalProperties?: OpenAPI2SchemaObject | OpenAPI2Reference | boolean;
+    allOf?: OpenAPI2SchemaObject[];
+    description?: string;
+    enum?: string[];
+    format?: string;
+    items?: OpenAPI2SchemaObject | OpenAPI2Reference;
+    oneOf?: (OpenAPI2SchemaObject | OpenAPI2Reference)[];
+    properties?: {
+        [index: string]: OpenAPI2SchemaObject | OpenAPI2Reference;
+    };
+    required?: string[];
+    title?: string;
+    type?: OpenAPI2Type;
+    [key: string]: any;
+}
+export {};
